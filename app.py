bfd100c2fc9ed0300cef7ac240f52db11b0138e5
C S Sanjay Kumar <chintalashobansanjaykumar@gmail.com>
2026-03-26 19:18:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-from flask import Flask
+from fastapi import FastAPI
 
-app = Flask(__name__)
+app = FastAPI()
 
 def add(a, b):
     return a + b
 
-@app.route("/")
+@app.get("/")
 def home():
-    return "Hello from DevOps Pipeline!"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    return {"message": "DevOps Pipeline Working!"}
